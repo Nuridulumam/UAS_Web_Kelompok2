a43a99323602e1710ce58126d8db7f3c721c128e
Nuridul Umam <56583725+Nuridulumam@users.noreply.github.com>
2021-06-09 14:58:14 +0700

    [UPDATE] view kategori berita

---
+++ app/Http/Controllers/KategoriberitaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kategoriberita;
+use Illuminate\Http\Request;
+
+class KategoriberitaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Kategoriberita  $kategoriberita
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kategoriberita $kategoriberita)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kategoriberita  $kategoriberita
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kategoriberita $kategoriberita)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Kategoriberita  $kategoriberita
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Kategoriberita $kategoriberita)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kategoriberita  $kategoriberita
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kategoriberita $kategoriberita)
+    {
+        //
+    }
+}
